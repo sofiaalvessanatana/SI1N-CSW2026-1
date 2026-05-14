04f85c7ba69dd2faf96a46a147efee48c38c9535
sofia santana <62352574+sofiaalvessanatana@users.noreply.github.com>
2026-05-13 22:34:18 -0300

    Add files via upload

---
+++ aula-14/javascript.js
@@ -0,0 +1,23 @@
+var x = 5;
+if (x > 5) {
+    console.log ("x is greater than 5");
+} else if (x < 5 ){
+    console.log ("x is less than or equal to 5");
+} else{
+    console.log ( "x is equal to 5")
+}
+
+for ( let i = 0; i < 10; i += 2) {  
+        console.log(i);  
+};
+
+for ( let i = 1; i < 10; i += 2) {
+    console.log(i);  1 ,3 ,5 , 7, 9
+};
+
+y = 0
+while ( y < 10 ) {
+    ( y % 2 === 0) ? console.log (y) : 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    y++; 
+
+}
