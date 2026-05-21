e862a8250d4f571645df3cae7381bee5020014a3
sofia santana <62352574+sofiaalvessanatana@users.noreply.github.com>
2026-05-20 22:32:25 -0300

    javascript.js

--- aula-16/javascript.js
+++ aula-16/javascript.js
@@ -3,7 +3,7 @@ document.writeln ("<h2> Bem-vindo ao javascript </h2>");
 window.alert("Esta é uma mensagem de alerta!");
 document.getElementById("title").innerHTML = "aprendendo JS com o melhor professor da uvv!!"
 
-//let nome = prompt ("digite seu nome:");
+let nome = "inserir" //prompt ("digite seu nome:");
 
 const novafrase = document.getElementById("novafrase");
 
